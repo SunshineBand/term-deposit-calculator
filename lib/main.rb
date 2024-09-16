c722dc93cb0b216f9b17bf5839dbb5d2da863959
SunshineBand <kyledchenoweth@gmail.com>
2024-09-16 17:50:43 +1000

    refactor: introduce compound interest formula

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,9 @@
 USAGE = 'Usage: calculator <amount> <interest> <term_years> <frequency>'.freeze
 
+def get_compounded_interest(principal, rate, frequency_per_year, years)
+  principal * ((1 + rate / frequency_per_year)**(frequency_per_year * years))
+end
+
 def main(inputs = ARGV)
   return puts USAGE if inputs.empty?
 
@@ -12,16 +16,11 @@ def main(inputs = ARGV)
 
   case frequency
   when 'maturity', 'at_maturity'
+    # maturity provides no compounded interest
     interest_earned = (amount.to_i * (interest.to_f / 100)) * term_in_years.to_i
     result = (amount.to_i + interest_earned).round
   when 'annually'
-    total = amount.to_f
-    interest_earned = 0
-    term_in_years.to_i.times do
-      interest_earned = (total * (interest.to_f / 100))
-      total += interest_earned
-    end
-    result = total.round
+    result = get_compounded_interest(amount.to_i, interest.to_f / 100, 1, 3).round
   end
 
   puts result
