66c876cfcdc109443d51828a153764e3c28def37
SunshineBand <kyledchenoweth@gmail.com>
2024-09-16 20:35:43 +1000

    refactor: extract compounding calculation to class

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,4 @@
-USAGE = 'Usage: calculator <princiap> <interest> <term_years> <frequency>'.freeze
+USAGE = 'Usage: calculator <principal> <interest> <term_years> <frequency>'.freeze
 
 def get_compounded_interest(principal, rate, frequency_per_year, years)
   (principal * ((1 + rate / frequency_per_year)**(frequency_per_year * years))).round
@@ -8,7 +8,8 @@ COMPOUNDING = {
   'monthly': 12,
   'quarterly': 4,
   'annually': 1,
-  'maturity': -1
+  'maturity': -1,
+  'at_maturity': -1
 }.freeze
 
 # Holds information for Interest calculation
@@ -17,39 +18,31 @@ class Interest
     @principal = principal.to_i
     @rate = rate.to_f
     @term = term.to_i
-    @frequency = COMPOUNDING.key? frequency ? COMPOUNDING[frequency] : nil
+    @frequency = COMPOUNDING.key?(frequency.to_sym) ? COMPOUNDING[frequency.to_sym] : nil
   end
 
   def valid?
     return false if @principal <= 0 || @term <= 0 || @rate <= 0
-    return false if @rate >= 15
     return false if @frequency.nil?
 
     true
   end
+
+  def compound
+    if @frequency == -1
+      interest_earned = (@principal.to_i * (@rate.to_f / 100)) * @term.to_i
+      return (@principal.to_i + interest_earned).round
+    end
+
+    get_compounded_interest(@principal.to_i, @rate.to_f / 100, @frequency, @term.to_i)
+  end
 end
 
 def main(inputs = ARGV)
-  # potential new feature
   return puts USAGE if inputs.empty? || inputs.size != 4
 
-  amount = inputs[0]
-  interest = inputs[1]
-  term_in_years = inputs[2]
-  frequency = inputs[3]
+  calculator = Interest.new(inputs[0], inputs[1], inputs[2], inputs[3])
+  return puts USAGE unless calculator.valid?
 
-  return puts USAGE unless Interest.new(inputs[0], inputs[1], inputs[2], inputs[3]).valid?
-
-  result = 'nope'
-
-  case frequency
-  when 'maturity', 'at_maturity'
-    # maturity provides no compounded interest
-    interest_earned = (amount.to_i * (interest.to_f / 100)) * term_in_years.to_i
-    result = (amount.to_i + interest_earned).round
-  else
-    result = get_compounded_interest(amount.to_i, interest.to_f / 100, COMPOUNDING[frequency.to_sym], term_in_years.to_i)
-  end
-
-  puts result
+  puts calculator.compound
 end
